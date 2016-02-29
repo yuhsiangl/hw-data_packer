8f9f10c7f55accde6408c306fc2a0d0d189d1da1
Soumya Ray <soumya.ray@gmail.com>
2016-02-29 14:13:57 +0800

    Create empty code template

---
+++ short_string_packer.rb
@@ -0,0 +1,17 @@
+class ShortStringPacker
+  ## Packs a short string into a Fixnum
+  # Arguments:
+  #   str - String object
+  # Returns: a Fixnum object
+  def pack(str)
+    # IMPLEMENT THIS METHOD
+  end
+
+  ## Unpacks a Fixnum from pack() method into a short string
+  # Arguments:
+  #   packed - a Fixnum object
+  # Returns: a String object
+  def unpack(packed)
+    # IMPLEMENT THIS METHOD
+  end
+end
