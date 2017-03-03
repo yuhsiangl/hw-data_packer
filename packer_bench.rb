a3c8a35f7bf848e2c38bd4490f8c092a6164a129
Soumya Ray <soumya.ray@gmail.com>
2017-03-03 18:15:26 +0800

    Fix: rubocop violations in benching code

--- packer_bench.rb
+++ packer_bench.rb
@@ -1,19 +1,19 @@
 require 'benchmark'
 require './short_string_packer'
 
-bench_results = Benchmark.bm do |bench|
+Benchmark.bm do |bench|
   letters = ('a'..'j').to_a.join
   packed = ShortStringPacker.pack(letters)
 
-  bench.report "pack" do
-    100_000.times {
+  bench.report 'pack' do
+    100_000.times do
       ShortStringPacker.pack(letters)
-    }
+    end
   end
 
-  bench.report "unpack" do
-    100_000.times {
+  bench.report 'unpack' do
+    100_000.times do
       ShortStringPacker.unpack(packed)
-    }
+    end
   end
 end
